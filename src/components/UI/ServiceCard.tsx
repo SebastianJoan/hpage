7c123107a827232feb634030cabf7507b088b75e
Joan Sebastián Giraldo Morales <sebastiangiraldo3@gmail.com>
2024-04-24 19:52:13 -0500

    fix error

--- src/components/UI/ServiceCard.tsx
+++ src/components/UI/ServiceCard.tsx
@@ -9,7 +9,7 @@ interface Props{
 
 export default function ServiceCard({ Nombre, Descripcion, ImageData }:Props) {
   return (
-    <div className='flex flex-col md:flex-row items-center justify-start w-full  bg-white h-[300px] shadow shadow-black rounded-md'>
+    <div className='flex flex-col md:flex-row items-center justify-start w-full h-auto bg-white md:h-[300px] shadow shadow-black rounded-md'>
         <div className="w-auto h-full">
             <Image
                 src={ImageData}
@@ -19,11 +19,11 @@ export default function ServiceCard({ Nombre, Descripcion, ImageData }:Props) {
                 alt="movie.title"
             />
         </div>
-        <div className="w-full h-full flex flex-col justify-center p-8">
-            <h4 className="text-2xl md:text-3xl lg:text-4xl font-bold text-start text-blue-500 mt-6">
+        <div className="w-full h-full flex flex-col justify-center p-4 gap-3">
+            <h4 className="text-2xl md:text-3xl lg:text-4xl font-bold text-start text-blue-500">
                 { Nombre }
             </h4>
-            <span className="text-xl md:text-xl lg:text-base text-justify  ">
+            <span className="text-base md:text-xl lg:text-base text-justify  ">
                 { Descripcion }
             </span>
         </div>
